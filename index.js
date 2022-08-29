13d0eefb045253f4cf1f7aab8fa0d24641f7a266
Bruno <brunochelimo@gmail.com>
2022-08-29 08:26:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // Write yoleur code in this file!
-const currentUser = 'Bruno';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = 'Bruno Kiptoo';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+const order = 'cheeseburger';
+
+let ingredients; 
+if (order === 'cheeseburger'){
+    ingredients = 'bun, burger, cheese, lettuce, tomato, onion';
+}
+else if (order === 'humburger'){
+    ingredients = 'bun, burger, lettuce, tomato, onion';
+
+}
+else if (order === 'malted'){
+ingredients = 'milk, ice cream, malted milk powder';
+}
+else {
+    console.log("Sorry, that's not on the menu right now.");
+}
